2075f22b40fb3c4d4b73c756c2f0c263d215f8bf
Huixiaqing Liu <desmond_liu_0619@163.com>
2024-06-06 20:38:14 +0100

    Replace absolute path with relative path

--- Tools/compute_dice.py
+++ Tools/compute_dice.py
@@ -27,7 +27,7 @@ def calculate_averages(file_path):
         return 0, 0
 
 
-file_path = 'C:/Users/huixi/OneDrive - Durham University/Final Project/2-Code/Data/results/unet/relu/split_1/dice_jacc_scores.csv'
+file_path = '../Data/results/unet/relu/split_1/dice_jacc_scores.csv'
 average_dice, average_jaccard = calculate_averages(file_path)
 print(f"Average Dice Coefficient: {average_dice}")
 print(f"Average Jaccard Index: {average_jaccard}")
